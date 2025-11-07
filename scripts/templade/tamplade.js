5a4e9d6c2716fadf736c2bda49f992737556bd06
Maik648 <maikpankow@gmail.com>
2025-11-07 05:26:17 +0100

    style templade

--- scripts/templade/tamplade.js
+++ scripts/templade/tamplade.js
@@ -13,11 +13,13 @@ function getCategoryHeader() {
 function getDeshesHeaderTemplade(categoryindex) {
   return `
     <div class="nav-section">
-        <img id="nav-img" src="../assets/img/icons/dm.png" alt="pfeil-Icon">
-        <img src="${category[categoryindex].img}" alt="category-icon">
-        <h3>${category[categoryindex].name}</h3>
-            <div class="bewertungen">
-                <p>Bewertungen: (${category[categoryindex].bewertung})</p>
+    <div class="nav-header">
+    <img id="nav-img" src="../assets/img/icons/dm.png" alt="pfeil-Icon">
+    <h3>${category[categoryindex].name}</h3>
+    <img src="${category[categoryindex].img}" alt="category-icon">
+    </div>
+    <div class="bewertungen">
+    <p>Bewertungen: (${category[categoryindex].bewertung})</p>
                 <img id="star-img" src="../assets/img/icons/stern.png" alt="star-Icon">
             </div>
 </div>`;
