43d88e1290ceb4029817c7fcccb56217b9037091
Maik648 <maikpankow@gmail.com>
2025-11-12 05:38:44 +0100

    new functions and basket

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
-
 function init() {
-    renderDeshesHeader();
+  renderDefaultDeshes();
+  renderBasket();
+  name();
 }
 
-function renderDeshesHeader() {
-  const mainContent = document.getElementById("menu-header");
+function renderDefaultDeshes() {
+  const mainContent = document.getElementById("deshes-content");
   mainContent.innerHTML = "";
-
-  for (
-    let categoryindex = 0; categoryindex < category.length; categoryindex++) {
-    let categorys = category[categoryindex];
-    mainContent.innerHTML += getDeshesMenuTemplade(categorys);
+  for (let index = 0; index < dishes.length; index++) {
+    const dishe = dishes[index];
+    mainContent.innerHTML += `<div id="dishes-card" class="dishes-card">
+                        <img src="${dishe.img}" alt="${dishe.name}">
+                        <p>${dishe.name}</p>
+                        <p>${dishe.description}</p>
+                        <p>${dishe.price.toFixed(2)} €</p>
+                        <button onclick="addDeshesToBasket(id, ${dishe.id})"class="add-to-basket-button">+</button>
+                    </div>`;
   }
 }
 
-
-function renderDeshes() {
-  const mainContent = document.getElementById("Content");
-  mainContent.innerHTML = "";
-
- 
+function renderBasket() {
+  const basketContent = document.getElementById("basket-content");
+  basketContent.innerHTML = "";
+        
+  for (let basketindex = 0; basketindex < baskets.length; basketindex++) {
+    const basket = baskets[basketindex];
+    basketContent.innerHTML += `
+       <button>+</button>
+        <span>${basket.id}</span>
+        <img src="${basket.img}" alt=""${basket.name}>
+        <button>-</button>
+  `;}
 }
 
+function addDeshesToBasket(id, index) {
+  if (dishes.length > 0) {
+    let newDishes = dishes[index];
+    baskets.push(newDishes);
+    renderBasket(baskets);
+  }
+}
+
+function name(basketindex) {
+  renderBasket(basketindex) ? "Warenkorb ist Leer" : renderBasket(basketindex);
+}; 
